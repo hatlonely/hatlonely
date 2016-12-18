87176bf5bbabe3c2bd43e66aeb021787840206d6
hatlonely <hatlonely@gmail.com>
2016-12-18 21:05:24 +0800

    update

--- hellowords/http/build.sh
+++ hellowords/http/build.sh
@@ -1,3 +1,30 @@
 #!/usr/bin/env bash
 
-go build src/main/hellowords.go
+function Build() {
+    go build src/main/hellowords.go
+}
+
+function Clean() {
+    rm -rf output hellowords
+}
+
+function Pack() {
+    mkdir -p output
+    mkdir -p output/data
+    Build && mv hellowords output && cp data/one.txt output/data/one.txt
+}
+
+function Help() {
+    echo "sh build.sh [pack | clean | build]"
+}
+
+function Main() {
+    case $1 in
+        "pack") Pack;;
+        "clean") Clean;;
+        "build") Build;;
+        *) Help;;
+    esac
+}
+
+Main $@
