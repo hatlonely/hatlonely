6b898e898f4d85a60e3411cdd1531cbf36435aef
hatlonely <hatlonely@gmail.com>
2016-12-21 00:35:12 +0800

    update

--- xiaoke/app.js
+++ xiaoke/app.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
 
-app.use('/', index);
+app.use('/', doc_list);
 app.use('/users', users);
 app.use('/services/doc_list', doc_list);
 app.use('/services/markdown', markdown);
