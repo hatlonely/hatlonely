72036f8bf24d7e0e628b5852ec247032b0ad4f19
hatlonely <hatlonely@gmail.com>
2016-12-30 00:14:40 +0800

    update

--- xiaoke/module/cache.js
+++ xiaoke/module/cache.js
@@ -1,33 +1,37 @@
-var mysql  = require('mysql');
+var mysql = require('mysql');
 var config = require('../config/xiaoke');
+var async = require('async');
 
 var cache = {}
 
-function init() {
-    var connection = mysql.createConnection(config['mysql']);
-    connection.connect(function(err) {
-        if (err) {
-            console.error('error connecting: ' + err.stack);
-            connection.destroy();
-            return;
-        }
-
+async.waterfall([
+    function(callback) {
+        var connection = mysql.createConnection(config['mysql']);
+        connection.connect(function(err) {
+            if (err) {
+                connection.destroy();
+            }
+            callback(err, connection);
+        });
+    },
+    function(connection, callback) {
         connection.query('SELECT * FROM docs', function(err, rows) {
             if (err) {
-                console.log('error query: ' + err.stack);
                 connection.destroy();
-                return;
             }
-
-            cache["docs"] = {}
-            for (i in rows) {
-                cache["docs"][rows[i].title] = rows[i].views
-            }
-            connection.destroy();
+            callback(err, rows);
         });
-    })
-}
+    }
+], function(err, rows) {
+    if (err) {
+        console.log('error query: ' + err.stack);
+        return;
+    }
 
-init();
+    cache["docs"] = {}
+    for (i in rows) {
+        cache["docs"][rows[i].title] = rows[i].views
+    }
+});
 
 module.exports = cache;
--- xiaoke/module/docs.js
+++ xiaoke/module/docs.js
@@ -1,47 +1,56 @@
-var mysql  = require('mysql');
+var mysql = require('mysql');
 var config = require('../config/xiaoke');
-var cache  = require('./cache');
+var cache = require('./cache');
 var crypto = require('crypto');
+var async = require('async');
 
 function md5(text) {
     return crypto.createHash('md5').update(text, 'utf-8').digest('hex');
 }
 
 var select = function(doc) {
-    var connection = mysql.createConnection(config['mysql']);
-
-    connection.connect(function(err) {
-        if (err) {
-            console.error('error connecting: ' + err.stack);
-            connection.destroy();
-            return;
-        }
-
-        connection.query('SELECT * FROM docs WHERE md5="' + md5(doc) + '"', function(err, rows) {
-            if (err) {
-                console.log('error query: ' + err.stack);
-                connection.destroy();
-                return;
-            }
-
-            sql = ''
-            if (rows.length == 0) {
-                cache["docs"][doc] = 1
-                sql = 'INSERT INTO docs (title, md5, views) VALUES ' +
-                    '("' + doc + '", "' + md5(doc) + '", 1)'
-            } else {
-                cache["docs"][rows[0].title] = rows[0].views + 1
-                sql = 'UPDATE docs SET views=views+1 WHERE md5="' + md5(doc) + '"'
-            }
-            connection.query(sql, function(err, rows) {
+    async.waterfall([
+        function(callback) {
+            var connection = mysql.createConnection(config['mysql']);
+            connection.connect(function(err) {
                 if (err) {
-                    console.error('exec failed: ' + sql + ' ' + err.stack);
-                } else {
-                    console.log('exec success: ' + sql);
+                    connection.destroy();
                 }
-                connection.destroy();
+                callback(err, connection)
             });
-        });
+        },
+        function(connection, callback) {
+            connection.query('SELECT * FROM docs WHERE md5=?', md5(doc), function(err, rows) {
+                if (err) {
+                    connection.destroy();
+                }
+                callback(err, {
+                    'rows': rows,
+                    'connection': connection
+                })
+            })
+        },
+        function(connrows, callback) {
+            connection = connrows['connection']
+            rows = connrows['rows']
+            if (rows.length == 0) {
+                cache['docs'][doc] = 1;
+                connection.query('INSERT INTO docs (title, md5, views) VALUES (?, ?, 1)', doc, md5(doc), function(err, rows) {
+                    callback(err, null);
+                    connection.destroy();
+                });
+            } else {
+                cache['docs'][doc] = rows[0].views + 1;
+                connection.query('UPDATE docs SET views=views+1 WHERE md5=?', md5(doc), function(err, rows) {
+                    callback(err, null);
+                    connection.destroy();
+                });
+            }
+        }
+    ], function(err, result) {
+        if (err) {
+            console.error(err);
+        }
     });
 }
 
