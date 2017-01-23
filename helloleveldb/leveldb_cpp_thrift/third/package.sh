d917536cd64ce6b85f844705dd1f3be4d54ba4ea
hatlonely <hatlonely@gmail.com>
2017-01-23 11:01:36 +0800

    update

--- helloleveldb/leveldb_cpp_thrift/third/package.sh
+++ helloleveldb/leveldb_cpp_thrift/third/package.sh
@@ -13,7 +13,7 @@ function Info() {
 function InstallLeveldb() {
     output="leveldb-v1.19"
     [[ -e $output ]] && Info "$output is exists" && return 0
-    mkdir $output && cd $output &&
+    mkdir -p $output/src && cd $output/src &&
     wget https://github.com/google/leveldb/archive/v1.19.zip -O $output.zip &&
     unzip $output.zip && cd leveldb-1.19 &&
     make -j8 && mkdir output && mkdir -p output/lib &&
@@ -26,7 +26,7 @@ function InstallLeveldb() {
 function InstallLibevent() {
     output="libevent-2.0.22"
     [[ -e $output ]] && Info "$output is exists" && return 0
-    mkdir $output && cd $output &&
+    mkdir $output/src && cd $output/src &&
     wget https://github.com/libevent/libevent/releases/download/release-2.0.22-stable/libevent-2.0.22-stable.tar.gz -O $output.tar.gz &&
     tar -xzvf $output.tar.gz && cd libevent-2.0.22-stable &&
     ./configure --prefix=$kWorkRoot/$output && make -j8 && make install && rm -rf output/lib/*.dylib &&
@@ -37,7 +37,7 @@ function InstallLibevent() {
 function InstallBison() {
     output="bison-2.5"
     [[ -e $output ]] && Info "$output is exists" && return 0
-    mkdir $output && cd $output &&
+    mkdir $output/src && cd $output/src &&
     wget http://ftp.gnu.org/gnu/bison/bison-2.5.tar.gz -O $output.tar.gz &&
     tar -xzvf $output.tar.gz && cd bison-2.5 &&
     ./configure --prefix=$kWorkRoot/$output && make -j8 && make install &&
@@ -49,7 +49,7 @@ function InstallThrift() {
     export PATH=$kWorkRoot/bison-2.5/bin:$PATH
     output="thrift-0.10.0"
     [[ -e $output ]] && Info "$output is exists" && return 0
-    mkdir $output && cd $output &&
+    mkdir $output/src && cd $output/src &&
     wget http://mirrors.cnnic.cn/apache/thrift/0.10.0/thrift-0.10.0.tar.gz -O $output.tar.gz &&
     tar -xzvf $output.tar.gz && thrift-0.10.0 &&
     ./configure CXXFLAGS='-g -O2' --prefix=$kWorkRoot/$output && make -j8 && make install &&
@@ -63,7 +63,7 @@ function InstallLibconfig() {
     mkdir -p $output/src && cd $output/src &&
     wget http://www.hyperrealm.com/libconfig/libconfig-1.5.tar.gz &&
     tar -xzvf libconfig-1.5.tar.gz && cd libconfig-1.5 &&
-    ./configure --prefix=`pwd`/../../ &&
+    ./configure --prefix=$kWorkRoot/$output &&
     make && make install &&
     Info "install $output succ" && return 0
     Warn "install $output fail" && return 255
@@ -75,7 +75,7 @@ function InstallLog4cplus() {
     mkdir -p $output/src && cd $output/src &&
     wget http://nchc.dl.sourceforge.net/project/log4cplus/log4cplus-stable/1.2.0/log4cplus-1.2.0.tar.xz &&
     tar -xvf log4cplus-1.2.0.tar.xz && cd log4cplus-1.2.0 &&
-    ./configure --prefix=`pwd`/../../ &&
+    ./configure --prefix=$kWorkRoot/$output &&
     make && make install &&
     Info "install $output succ" && return 0
     Warn "install $output fail" && return 255
@@ -87,7 +87,7 @@ function InstallGtest() {
     mkdir -p gtest-1.8.0/src && cd gtest-1.8.0/src &&
     wget https://codeload.github.com/google/googletest/zip/release-1.8.0 -O release-1.8.0.zip &&
     unzip release-1.8.0.zip && cd googletest-release-1.8.0 &&
-    mkdir build && cd build && cmake .. -DCMAKE_INSTALL_PREFIX=`pwd`/../../../ &&
+    mkdir build && cd build && cmake .. -DCMAKE_INSTALL_PREFIX=$kWorkRoot/$output &&
     make && make install &&
     Info "install $output succ" && return 0
     Warn "install $output fail" && return 255
